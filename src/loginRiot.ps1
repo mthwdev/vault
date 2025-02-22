5fafa464e774cb7124f699231ba782cede05449b
matthew <cleansernld@gmail.com>
2025-02-22 01:26:06 +0100

    fix login script not working as intented (#2)

--- src/loginRiot.ps1
+++ src/loginRiot.ps1
@@ -45,10 +45,16 @@ Add-Type -AssemblyName System.Windows.Forms
 Write-Output "-----"
 Write-Output "executing powershell script"
 
-$process = Get-Process | Where-Object { $_.MainWindowTitle -like "*Riot*" } | Select-Object -First 1
+$process = Get-Process | Where-Object { $_.MainWindowTitle -like "*Riot Client*" } | Select-Object -First 1
 if ($process) {
     Write-Output "found window '$($process.MainWindowTitle)'"
     $hwnd = $process.MainWindowHandle
+    
+    [Win32]::ShowWindow($hwnd, 9) | Out-Null
+    Start-Sleep -Milliseconds 100
+    
+    [Win32]::SetForegroundWindow($hwnd) | Out-Null
+    Start-Sleep -Milliseconds 100
     
     $rect = New-Object Win32+RECT
     [Win32]::GetWindowRect($hwnd, [ref]$rect) | Out-Null
@@ -56,51 +62,52 @@ if ($process) {
     $height = $rect.Bottom - $rect.Top
     
     $screen = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea
-    
     $x = [Math]::Max(0, [Math]::Floor(($screen.Width - $width) / 2) + $screen.Left)
     $y = [Math]::Max(0, [Math]::Floor(($screen.Height - $height) / 2) + $screen.Top)
     
     Write-Output "moving window to X=$x, Y=$y (Width=$width, Height=$height)"
     
     [Win32]::MoveWindow($hwnd, $x, $y, $width, $height, $true) | Out-Null
-    [Win32]::ShowWindow($hwnd, 9) | Out-Null
-    [Win32]::SetForegroundWindow($hwnd) | Out-Null
     
-    $centerX = $x + ($width / 2)
-    $centerY = $y + ($height / 2)
-
-    Start-Sleep -Milliseconds 50
-    
-    Write-Output "clicking center of window: X=$centerX Y=$centerY"
-    [Win32]::SetCursorPos([int]$centerX, [int]$centerY) | Out-Null
-    [Win32]::mouse_event(0x0002, 0, 0, 0, 0)
-    [Win32]::mouse_event(0x0004, 0, 0, 0, 0)
+    $clickX = $x + 75
+    $clickY = $y + 75
 
     Start-Sleep -Milliseconds 100
+    
+    Write-Output "clicking near top-left of window: X=$clickX Y=$clickY"
+    for ($i = 0; $i -lt 3; $i++) {
+        Start-Sleep -Milliseconds 50
+        [Win32]::SetCursorPos([int]$clickX, [int]$clickY) | Out-Null
+        [Win32]::mouse_event(0x0002, 0, 0, 0, 0)
+        [Win32]::mouse_event(0x0004, 0, 0, 0, 0)
+    }
+
+    Start-Sleep -Milliseconds 50
 
     Write-Output "pressing tab 3 times"
     for ($i = 0; $i -lt 3; $i++) {
+        Start-Sleep -Milliseconds 50
         [Win32]::keybd_event(0x09, 0, 0, 0)
         [Win32]::keybd_event(0x09, 0, 2, 0)
     }
 
-    Start-Sleep -Milliseconds 100
+    Start-Sleep -Milliseconds 50
     
     Write-Output "entering username"
     [System.Windows.Forms.SendKeys]::SendWait($username)
 
-    Start-Sleep -Milliseconds 100
+    Start-Sleep -Milliseconds 50
     
     Write-Output "pressing tab"
     [Win32]::keybd_event(0x09, 0, 0, 0)
     [Win32]::keybd_event(0x09, 0, 2, 0)
 
-    Start-Sleep -Milliseconds 100
+    Start-Sleep -Milliseconds 50
     
     Write-Output "entering password"
     [System.Windows.Forms.SendKeys]::SendWait($password)
 
-    Start-Sleep -Milliseconds 100
+    Start-Sleep -Milliseconds 50
     
     Write-Output "pressing enter"
     [Win32]::keybd_event(0x0D, 0, 0, 0)
